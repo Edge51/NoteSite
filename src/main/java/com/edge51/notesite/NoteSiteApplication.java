a5ca639c437ba003720565f95d92802c13f9d00d
Edge51 <liuyuanyuanedge@163.com>
2024-09-08 00:23:44 +0800

    Init Spring Project

---
+++ src/main/java/com/edge51/notesite/NoteSiteApplication.java
@@ -0,0 +1,13 @@
+package com.edge51.notesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edge51/notesite/NoteSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edge51.notesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
